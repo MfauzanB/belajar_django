aeb40b570bf039e1fc08e5f0470bd508ecc48f12
MfauzanB <mfauzan652.mfb@gmail.com>
2021-09-12 14:11:12 +0700

    edit v=file views, dan model App articles

--- articles/form.py
+++
@@ -1,7 +0,0 @@
-from django import form
-from . import models
-
-class CreateArticles(forms.ModelForm):
-    class meta:
-        model = models.article
-        fields = ['title', 'body', 'slug', 'thumb']
